79c1ea926c246ce13467f8b9e1cd6dcfe4604805
vaisali chinta <vaisalichinta@gmail.com>
2025-08-01 06:30:28 +0530

    Added Comments.java

---
+++ 01_Basics/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        //Single line comments
+       System.out.println("This is the\n// single line comments"); 
+
+       /*Multi 
+            line 
+              Comments*/
+       System.out.println("This is the \n/* Multi\nline \nComments*/");
+    }
+}
