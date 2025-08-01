063bd420affb4df2ed5194c04a88bf1d5195997d
vaisali chinta <vaisalichinta@gmail.com>
2025-08-01 06:30:29 +0530

    Added Variables.java

---
+++ 01_Basics/Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+     public static void main(String[] args) {
+        //Rules to name a variable 
+        System.out.println();
+
+        // Declaring a variable
+        int myNum; 
+
+        // Intialization of a variable
+        myNum = 21; 
+        System.out.println(myNum);
+
+        // Declaring and Intializing the variable
+        int number_Of_Digits= 21;
+        
+        //Assigning a new value to existing variable,it will overwite the previous value
+        number_Of_Digits = 25;
+        System.out.println(number_Of_Digits);// Now number_of_digits is 25
+
+        // final keyword is used when the value is unchangeable and read-only
+        final float pi = 3.14f;
+        System.out.println(pi);
+        
+    
+
+    }
+}
