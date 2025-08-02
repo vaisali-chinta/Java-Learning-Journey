e725a17bce05d5d8f670306e03c893d0a7a09eae
vaisali chinta <vaisalichinta@gmail.com>
2025-08-02 23:04:23 +0530

    Added Datatype.java

---
+++ 01_Basics/Datatype.java
@@ -0,0 +1,44 @@
+public class Datatype {
+    public static void main(String[] args) {
+        // primitive datatypes
+
+        String name = "Rosy";
+        //datatype : string
+
+        int age = 21;
+        //datatype : int and size of 4 bytes
+
+        byte roll_no = 32;
+        //datatype :byte and size of 1 byte
+
+        float cgpa = 9.86f;
+        //datatype : float and and size of 4 bytes & ranges upto 7 decimal points
+
+        double percentage = 80.90d;
+        //datatype : double and and size of 8 bytes & ranges upto 16 decimal points
+
+        char grade = 'O';
+        //datatype : char and and size of 2 bytes
+
+        boolean student = true;
+        //datatype : bool and size of 1 byte
+
+        short bus_fee = 22500;
+        //datatype : short and size of 2 bytes
+
+        long total_fee = 3100000L;
+        //datatype : long and size of 8 bytes
+
+        // printing 
+        System.out.println("Details : ");
+        System.out.println("Name of the student : "+name);
+        System.out.println("Age of the student : "+age);
+        System.out.println("Roll Number of the student : "+roll_no);
+        System.out.println("Cgpa of the student : "+cgpa);
+        System.out.println("percentage of the student : "+percentage);
+        System.out.println("Grade of the student : "+grade);
+        System.out.println("Is student data true : "+student);
+        System.out.println("Bus fee for the semester is :"+bus_fee);
+        System.out.println("Total fee for the 4 years is :"+total_fee);
+    }
+}
