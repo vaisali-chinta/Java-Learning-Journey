5e880c14f44b76fc32a6c1a2b6eba80a77017d01
vaisali chinta <vaisalichinta@gmail.com>
2025-08-03 19:52:49 +0530

    Added Arithmetic_Operators.java

---
+++ 02_Operators/Arithmetic_Operators.java
@@ -0,0 +1,17 @@
+public class Arithmetic_Operators {
+    public static void main(String[] args) {
+        
+    //Arithmetic operators are used to perform common mathematical operations
+
+    int num_1 = 10;
+    int num_2 = 5;
+
+    System.out.println("Addition operator(+) adds two values : "+num_1+" + "+num_2+" = "+(num_1 + num_2));
+    System.out.println("Subtraction operator(-) subtracts one value from another : "+num_1+" - "+num_2+" = "+(num_1 - num_2));
+    System.out.println("Multiplication operator(*) multiplies two values : "+num_1+" * "+num_2+" = "+(num_1 * num_2));
+    System.out.println("Division operator(/) divides one value by another : "+num_1+" / "+num_2+" = "+(num_1 / num_2));
+    System.out.println("Modulus operator(%) returns the remainder : "+num_1+" % "+num_2+" = "+(num_1 % num_2));
+    System.out.println("Increment operator(++) increases the value by 1 : "+"++"+num_1+" = "+(++num_1));
+    System.out.println("Decrement operator(--) decreases the value by 1 : "+"--"+num_2+" = "+(--num_2));
+    }
+}
