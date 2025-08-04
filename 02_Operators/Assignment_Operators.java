7ed337bb4d6a3b7c9eaa0ee20bac75187c0476d6
vaisali chinta <vaisalichinta@gmail.com>
2025-08-04 20:42:36 +0530

    Added Assignment_Operators.java

---
+++ 02_Operators/Assignment_Operators.java
@@ -0,0 +1,30 @@
+public class Assignment_Operators {
+    public static void main(String[] args) {
+    int x = 5;
+    //Assignment operators are used to assign values to variables.
+    
+    System.out.println("Assignment operators :");
+    x = 5;
+    System.out.println("(+=)operator means : "+"x = x + 3"+" -> "+(x+=3));
+    x = 5;
+    System.out.println("(-=)operator means : "+"x = x - 3"+" -> "+(x-=3));
+    x = 5;
+    System.out.println("(*=)operator means : "+"x = x * 3"+" -> "+(x*=3));
+    x = 5;
+    System.out.println("(/=)operator means : "+"x = x / 3"+" -> "+(x/=3));
+    x = 5;
+    System.out.println("(%=)operator means : "+"x = x % 3"+" -> "+(x%=3));
+    x = 5;
+    System.out.println("(&=)operator means : "+"x = x & 3"+" -> "+(x&=3));
+    x = 5; 
+    System.out.println("(|=)operator means : "+"x = x | 3"+" -> "+(x|=3));
+    x = 5;
+    System.out.println("(^=)operator means : "+"x = x ^ 3"+" -> "+(x^=3));
+    x = 5;
+    System.out.println("(>>=)operator means : "+"x = x >> 3"+" -> "+(x>>=3));
+    x = 5;
+    System.out.println("(<<=)operator means : "+"x = x << 3"+" -> "+(x<<=3));
+     
+    
+}
+}
