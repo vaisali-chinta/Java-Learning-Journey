12ee4ceaa9ba2c8f65222282b9d5f0e9d6ff7b11
vaisali chinta <vaisalichinta@gmail.com>
2025-08-05 22:59:35 +0530

    Added Comparison_Operators.java

---
+++ 02_Operators/Comparison_Operators.java
@@ -0,0 +1,21 @@
+public class Comparison_Operators{
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 8;
+
+       /*Comparison operators are used to compare two values (or variables).
+        The return value of a comparison is either true or false.*/
+
+        System.out.println("(==)Equal to for : "+a+" == "+b+" is "+(a==b));
+        System.out.println("(!=)Not Equal to for : "+a+" != "+b+" is "+(a!=b));
+        System.out.println("(>)Greater than for : "+a+" > "+b+" is "+(a>b));
+        System.out.println("(<)Less than for : "+a+" < "+b+" is "+(a<b));
+        System.out.println("(>=)Greater than or Equal to for : "+a+" >= "+b+" is "+(a>=b));
+        System.out.println("(<=)Less than or Equal to for : "+a+" <= "+b+" is "+(a<=b));
+
+
+
+
+
+    }
+}
