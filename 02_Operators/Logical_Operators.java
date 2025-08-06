be448a7910dd2145810256066f4fe57a5e8e43a1
vaisali chinta <vaisalichinta@gmail.com>
2025-08-06 22:04:02 +0530

    "Added Logical_Operators.java

--- 02_Operators/Logical_Operators.java
+++ 02_Operators/Logical_Operators.java
@@ -7,7 +7,5 @@ public class Logical_Operators {
         System.out.println("(&&)Logical and returns true if both statements are true :"+(x < 7 && x < 12));
         System.out.println("(||)Logical or returns true if one of the statements is true :"+(x < 7 || x < 4));
         System.out.println("(!)Logical not reverses the result, returns false if the result is true :"+!(x < 5 && x < 10));
-        
-        
     }
 }
