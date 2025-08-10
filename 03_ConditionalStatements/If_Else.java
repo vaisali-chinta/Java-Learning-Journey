f46e0b37512d600dc8301dcce9e817527d1e4e17
vaisali chinta <vaisalichinta@gmail.com>
2025-08-10 18:02:30 +0530

    Added If_Else.java

--- 03_ConditionalStatements/If_Else.java
+++ 03_ConditionalStatements/If_Else.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class If_Else {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        //If- else statements
+
+        if(age > 18){
+            System.out.println("Adult");
+        } else {
+            System.out.println("Not Adult");
+        }
+        sc.close();
     }
 }
