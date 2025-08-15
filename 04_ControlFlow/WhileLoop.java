9634c4ebafc28b98e5a7689d07a3112c32811d16
vaisali chinta <vaisalichinta@gmail.com>
2025-08-15 17:00:06 +0530

    Added WhileLoop.java

---
+++ 04_ControlFlow/WhileLoop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class WhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //while loop
+
+        int i = 1;
+        while(i <= 15){
+            System.out.print(i+" ");
+            i++;
+        }
+        sc.close();
+    }
+}
