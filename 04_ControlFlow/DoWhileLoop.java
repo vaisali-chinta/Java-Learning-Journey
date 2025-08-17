bb53b6a4cc2282bf713b05a63e67c9a20b61fb75
vaisali chinta <vaisalichinta@gmail.com>
2025-08-17 18:09:04 +0530

    Added DoWhileLoop.java

---
+++ 04_ControlFlow/DoWhileLoop.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class DoWhileLoop{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Do while loop
+
+      int i = 12;
+      do{
+        System.out.print(i+" ");
+        i++;
+      } while(i < 11);
+      
+      sc.close();
+    }
+}
