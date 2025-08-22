65574fab9c1526978c56ef2fbe722585fb327e9a
vaisali chinta <vaisalichinta@gmail.com>
2025-08-22 10:24:41 +0530

    Added Number Checker Program

---
+++ Mini-Projects/NumberChecker.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class OddEvenChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number : ");
+        int number = sc.nextInt();
+
+        //OddEvenChecker
+        
+        if(number % 2 == 0){
+            System.out.println(number+" is Even Number");
+        } else {
+            System.out.println(number+" is Odd Number");
+        } 
+
+        //PrimeChecker
+        if(number <=1){
+            System.out.println(number+" is neither prime nor Composite Number");
+        } else {
+            boolean isPrime = true;
+            for(int i = 2 ; i <= Math.sqrt(number);i++){
+                if(number % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.println(number+" is Prime Number");
+            } 
+        }
+        sc.close();
+    } 
+}
