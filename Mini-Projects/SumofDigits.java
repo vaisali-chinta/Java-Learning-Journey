d0a211c77d42302e5e77d590e2adc2b7e3bc1fee
vaisali chinta <vaisalichinta@gmail.com>
2025-09-17 22:27:52 +0530

    Sum of Digits

---
+++ Mini-Projects/SumofDigits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumofDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("Sum of digits = " + sum);
+        sc.close();
+    }
+}
