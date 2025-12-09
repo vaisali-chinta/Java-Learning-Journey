cdbe3c7fe70894b581a9b08ffb41261b2ea1d87b
vaisali chinta <vaisalichinta@gmail.com>
2025-12-09 11:32:18 +0530

    Basic Projects

--- 01_Basics/Comments.java
+++ 01_Basics/Comments.java
@@ -2,7 +2,7 @@ public class Comments {
     public static void main(String[] args) {
 
         //Single line comments
-       System.out.println("This is the\n// single line comments"); 
+       System.out.println("This is the\n// single line comments//"); 
 
        /*Multi 
             line 
